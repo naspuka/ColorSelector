7f1719346ce8d4d41977774834ae47b4ee689508
Nasrullah Olajide <folanas10@gmail.com>
2024-10-15 17:17:34 +0100

    Initial Commit

---
+++ ColorSelector/UIColor+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Ext.swift
+//  ColorSelector
+//
+//  Created by Nasrullah Olajide on 15/10/2024.
+//
+
+import UIKit
+
+
+extension UIColor {
+    
+    static func random() -> UIColor {
+        let randomColor = UIColor(
+            red: CGFloat.random(in: 0...1),
+            green: CGFloat.random(in: 0...1),
+            blue: CGFloat.random(in: 0...1),
+            alpha: 1
+        )
+        return randomColor
+    }
+}
